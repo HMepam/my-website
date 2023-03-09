ad334eb828743a27c439b0a91f503dfa3ce8ff28
Hayk Malkhasyan <hayk_malkhasyan@epam.com>
2023-03-09 15:34:26 +0100

    fix(menu style): fix menu block style, add lins

--- blocks/menu/menu.js
+++ blocks/menu/menu.js
@@ -6,19 +6,30 @@ export default function decorate(block) {
   [...block.children].forEach((row) => {
     const li = document.createElement('li');
     li.innerHTML = row.innerHTML;
+    let link;
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) {
+        if (div.children[0].hasAttribute('href')) {
+          link = div.children[0].href;
+        }
         div.className = 'menu-item-icon';
+      } else if (div.children.length === 0) {
+        div.className = 'display-none';
       } else {
         div.className = 'menu-item-body';
       }
+    });
+    li.addEventListener('click', () => {
+      window.location.href = link;
     });
     ul.append(li);
   });
   ul.querySelectorAll('img')
     .forEach((img) => (
       img.closest('picture')
-        .replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ with: 1 }]))
+        .replaceWith(
+          createOptimizedPicture(img.src, img.alt, false, [{ with: 1 }]),
+        )
     ));
   block.textContent = '';
   block.append(ul);
