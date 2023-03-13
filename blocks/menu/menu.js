79348d9dae86a602c927b14ec2ae5d864ea38174
Hayk Malkhasyan <hayk_malkhasyan@epam.com>
2023-03-13 10:25:30 +0100

    fix(menu) fix style add mobile view

--- blocks/menu/menu.js
+++ blocks/menu/menu.js
@@ -1,7 +1,27 @@
 import { createOptimizedPicture } from '../../scripts/lib-franklin.js';
 
 export default function decorate(block) {
-  /* change to ul, li */
+  const logo = block.firstElementChild.children[0].cloneNode(true);
+  logo.className = 'logo';
+  const menuIcon = block.lastElementChild.children[0].cloneNode(true);
+  menuIcon.className = 'icon';
+  const hamburger = document.createElement('div');
+  hamburger.className = 'hamburger';
+  const iconWrapper = document.createElement('div');
+  iconWrapper.className = 'icon-wrapper';
+  iconWrapper.append(menuIcon);
+  const resources = document.createElement('div');
+  resources.className = 'resources';
+  const span = document.createElement('span');
+  resources.append(span);
+  span.innerText = 'RESOURCES';
+  hamburger.append(resources, iconWrapper);
+  block.removeChild(block.lastElementChild);
+
+  const mobileMenu = document.createElement('div');
+  mobileMenu.className = 'mobile-menu';
+  mobileMenu.append(logo, hamburger);
+
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
     const div = document.createElement('div');
@@ -36,6 +56,39 @@ export default function decorate(block) {
           createOptimizedPicture(img.src, img.alt, false, [{ with: 1 }]),
         )
     ));
+
+  const mobileView = document.createElement('div');
+  mobileView.className = 'mobile';
+  const desktopView = document.createElement('div');
+  desktopView.className = 'desktop';
+
+  const menuList = document.createElement('div');
+  menuList.className = 'list';
+
+  const listCover = document.createElement('div');
+  const listWrapper = document.createElement('div');
+  listCover.className = 'list-cover';
+  listWrapper.className = 'list-wrapper';
+
+  listCover.addEventListener('click', () => {
+    const body = document.getElementsByTagName('body');
+    body.classList.remove('modal-open');
+    const list = document.getElementsByClassName('list-wrapper');
+    list.style.display = 'none';
+  });
+
+  hamburger.addEventListener('click', () => {
+    const body = document.getElementsByTagName('body');
+    const list = document.getElementsByClassName('list-wrapper');
+    list.style.display = '';
+    body.classList.add('modal-open');
+  });
+
+  desktopView.append(ul.cloneNode(true));
+  menuList.append(ul.cloneNode(true));
+  listWrapper.append(menuList, listCover);
+  mobileView.append(mobileMenu, listWrapper);
+
   block.textContent = '';
-  block.append(ul);
+  block.append(desktopView, mobileView);
 }
